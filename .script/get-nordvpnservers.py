54ca360e45d582712913a3e39e933beb90e1c68d
Plug03 <arkady.varshamov@gmail.com>
2022-05-09 15:31:23 +0300

    update

--- .script/get-nordvpnservers.py
+++ .script/get-nordvpnservers.py
@@ -3,16 +3,16 @@ import json
 import pandas as pd
 
 # Download Nord Server list using API
-r  = requests.get('https://api.nordvpn.com/server')
+r = requests.get('https://api.nordvpn.com/server')
 
-#Decode byte array into string
+# Decode byte array into string
 my_json = r.content.decode('utf8')
 
-#Convert string to JSON
+# Convert string to JSON
 data = json.loads(my_json)
 
 # Load list of JSON records into dataframe
 df = pd.DataFrame(data)
 
 # Convert to csv
-df.to_csv('nordvpn-servers.csv',  index=False)
+df.to_csv('nordvpn-servers.csv', index=False)
